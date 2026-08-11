21a268dce27e3b7220b02bef48401c1a1f7da40f
ABC <80405362+q1016390482@users.noreply.github.com>
2026-08-11 10:59:48 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -1,18 +1,27 @@
 #!/bin/bash
 
-# 删除原有 passwall 相关 feed
+# 删除 LEDE 自带相关 feed，避免重复
 sed -i '/passwall/d' feeds.conf.default
+sed -i '/openclash/d'
+
 
 # 添加 PassWall 官方仓库
+
 echo 'src-git passwall_packages https://github.com/Openwrt-Passwall/openwrt-passwall-packages.git;main' >> feeds.conf.default
+
 echo 'src-git passwall_luci https://github.com/Openwrt-Passwall/openwrt-passwall.git;main' >> feeds.conf.default
 
 
-# 添加 OpenClash
-echo 'src-git openclash https://github.com/vernesong/OpenClash.git' >> feeds.conf.default
+# 添加 OpenClash 官方仓库
+
+echo 'src-git openclash https://github.com/vernesong/OpenClash.git;master' >> feeds.conf.default
 
 
 # 添加 Clashoo
+
 echo 'src-git clashoo https://github.com/kenzok8/openwrt-clashoo.git' >> feeds.conf.default
-# 晶晨宝盒
+
+
+# 添加晶晨宝盒
+
 git clone --depth=1 https://github.com/ophub/luci-app-amlogic.git package/luci-app-amlogic
